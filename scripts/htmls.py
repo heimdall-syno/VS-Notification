96643c587237bf2c6a5e114eb445623d335d4add
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2020-06-07 17:39:58 +0200

    Support for non-imdb items

--- scripts/htmls.py
+++ scripts/htmls.py
@@ -52,7 +52,7 @@ def html_add_category(content, category):
         '<font color="black"><h4>{category}:</h4></font>'.format(category=category)
     return content
 
-def html_add_item_image(content, movie, season, name, url):
+def html_add_item(content, movie, season, name, url, f4k):
     """ Add a item with an image next to the title.
 
     Arguments:
@@ -67,52 +67,31 @@ def html_add_item_image(content, movie, season, name, url):
 
     name = "{} {}".format(name, season)
     name =  "<br/>".join(textwrap.wrap(name, width=25))
-    rating = " ({})".format(movie["rating"]) if "rating" in movie else ""
+    rating = " ({})".format(movie["rating"]) if movie and "rating" in movie else ""
+    f4k = "- 4K" if f4k == True else ""
+    cover = movie['cover url'] if movie and 'cover url' in movie else "https://thetvdb.com/images/missing/movie.jpg"
+
+    ## Imdb URL or plain text
+    if url:
+        html_url = '''<a style="color: #000000; text-decorations:none;" href="{movie_url}">
+                    {name} {rating} {f4k}
+                 </a>'''.format(movie_url=url, name=name, rating=rating, f4k=f4k)
+    else:
+        html_url = '''{name} {rating} {f4k}'''.format(name=name, rating=rating, f4k=f4k)
+
     content = content + ''' \
     <div style="display: table; height: 70px; overflow: hidden;">
-        <div id="image" style="display:inline; padding-left: 30px;">
-            <img src="{movie_image_url}" width="65px"/>
-        </div>
+        <a href="{url}">
+            <div id="image" style="display:inline; padding-left: 30px;">
+                <img src="{cover}" width="65px"/>
+            </div>
+        </a>
         <div style="padding-left: 30px; display: table-cell; vertical-align: middle;">
             <div><font color="black"><strong><span>
-                <a style="color: #000000; text-decorations:none;" href="{movie_url}">
-                    {name} {rating}
-                </a>
+                {html_url}
             </span></strong></font></div>
         </div>
-    </div><p></p>'''.format(movie_image_url=movie['cover url'], movie_url=url,
-                            name=name, rating=rating)
-    return content
-
-def html_add_item_without_image(content, movie, season, name, url):
-    """ Add a item with an image next to the title.
-
-    Arguments:
-        content {string}  -- HTML content as string.
-        movie {string}    -- Movie instance of IMDbPy.
-        season {string}   -- Name of the season if it is a series.
-        name {string}     -- Title of the item.
-        url {string}      -- IMDB url of the item.
-    Returns:
-        string -- Updated HTML content as string.
-    """
-
-    name = "{} {}".format(name, season)
-    name =  "<br/>".join(textwrap.wrap(name, width=25))
-    rating = " ({})".format(movie["rating"]) if "rating" in movie else ""
-    content = content + ''' \
-    <div style="display: table; height: 70px; overflow: hidden;">
-        <div id="image" style="display:inline;padding-left: 30px;">
-            <img src="data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=" width="65px"/>
-        </div>
-    <div style="padding-left: 30px; display: table-cell; vertical-align: middle;">
-        <div><font color="black"><strong><span>
-            <a style="color: #000000; text-decorations:none;" href="{movie_url}">
-                {movie_name} {rating}
-            </a>
-        </span></strong></font></div>
-        </div>
-    </div><p></p>'''.format(movie_url=url, movie_name=name, rating=rating)
+    </div><p></p>'''.format(url=url, cover=cover, html_url=html_url)
     return content
 
 def html_add_items(content, category, items):
@@ -132,10 +111,16 @@ def html_add_items(content, category, items):
 
     history = []
     for movie_name in items:
+
+        ## Add information whether its 4K or not
+        f4k = ["4K", "2160p", "UHD", "HDR"]
+        format_4k = True if any(u for u in f4k if u in movie_name) else False
+
         ## Series name correction
         season = ""
         if (os.path.basename(category) != "Filme"):
             movie_name, season = movie_name.replace(category, "").split(os.sep)[1:3]
+            movie_name = movie_name.replace("oe", "ö").replace("ue", "ü").replace("ae", "ä")
             season = "- {} ".format(season)
             movie_season = "{}{}".format(movie_name, season)
 
@@ -143,10 +128,6 @@ def html_add_items(content, category, items):
         else:
             movie_name = movie_get_name(category, os.path.dirname(movie_name))
             movie_name = movie_name.replace("oe", "ö").replace("ue", "ü").replace("ae", "ä")
-            ultra_hd = ["4K", "2160p", "UHD", "HDR"]
-            if any(u for u in ultra_hd if u in movie_name):
-                infomsg("Skip entry due to 4K resolution", "HTML", (movie_name,))
-                continue
 
         ## Check for duplicates
         if (os.path.basename(category) == "Filme" and movie_name in history):
@@ -162,16 +143,16 @@ def html_add_items(content, category, items):
             movieID = search_results[0].movieID
             movie = access.get_movie(movieID)
             movie_url = access.get_imdbURL(movie)
-            if movie:
-                if 'cover url' in movie:
-                    content = html_add_item_image(content, movie, season, movie_name, movie_url)
-                    infomsg("Added image entry for entry", "HTML", (movie_name, movie, season))
-                else:
-                    content = html_add_item_without_image(content, movie, season, movie_name, movie_url)
-                    infomsg("Added plain entry for entry", "HTML", (movie_name, movie, season))
-                if (os.path.basename(category) == "Filme"):
-                    history.append(movie_name)
-                else:
-                    history.append(movie_season)
+            content = html_add_item(content, movie, season, movie_name, movie_url, format_4k)
+            infomsg("Added imdb entry (with image) for item", "HTML", (movie_name, movie, season))
+        else:
+            content = html_add_item(content, None, season, movie_name, None, format_4k)
+            infomsg("Add non-imdb entry for item", "HTML", (movie_name, season))
+
+        if (os.path.basename(category) == "Filme"):
+            history.append(movie_name)
+        else:
+            history.append(movie_season)
+
     content = content + '<p>&nbsp;</p>'
     return content
